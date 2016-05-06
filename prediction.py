76433c636d3406bbee1440a9a58c96e0d148e92c
RomainWarlop <romainwarlop@gmail.com>
2016-05-06 10:37:07 +0200

    start of code

---
+++ prediction.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import json
+from scipy.stats import entropy
+
+
+path = '/home/romain/Documents/BitBucket/DataForGood/teenspirit/teenspirit/'
+json_data=open(path+"dataset.json").read()
+data = json.loads(json_data)
+
+def movingaverage(interval, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    ma = np.convolve(interval, window, 'full')
+    ma = ma[:len(interval)]
+    ma[0] = interval[0]
+    return ma
+
+def timeSerieFeatures(x,M):
+    mn = np.mean(x)
+    std = np.std(x)
+    ent = entropy(x)
+    mean_momentum = np.mean(x-movingaverage(x,M))
+    return mn, std, ent, mean_momentum
