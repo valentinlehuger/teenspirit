e51aaf61987b3a792dcef4dd8bfca4e9f178b0fb
Valentin Lehuger <vlehuger@student.42.fr>
2016-05-29 12:51:45 +0200

    add user tweets task

--- bluebirdlib/tasks.py
+++ bluebirdlib/tasks.py
@@ -1,6 +1,7 @@
 from mrq.task import Task
 from bluebirdlib.connection import get_api
 from bluebirdlib.status import getTweetFromHashTag
+from bluebirdlib.user import userTimeline
 from bluebirdlib.data import add_to_mongo, is_exists
 
 import pprint
@@ -14,14 +15,26 @@ class FetchTwitterTask(Task):
             raise Exception("Impossible to get the api")
 
 
-class GetHashTagTweet(FetchTwitterTask):
+class GetHashTagTweets(FetchTwitterTask):
 
     def run(self, params):
         search = params.get("search", None)
         if not search:
-            raise Exception("Search not provide")
+            raise Exception("Search not provide.")
 
         tweets = map(lambda x: x.AsDict(), getTweetFromHashTag(search, self.api))
         pprint.PrettyPrinter(indent=2).pprint(tweets)
+        map(lambda x: add_to_mongo(x) if not is_exists(x["id"]) else None, tweets)
+        return 0
+
+
+class GetUserTweets(FetchTwitterTask):
+
+    def run(self, params):
+        user = params.get("user", None)
+        if not user:
+            raise Exception("User id not provide.")
+
+        tweets = map(lambda x: x.AsDict(), userTimeline(user, self.api, rec=1))
         map(lambda x: add_to_mongo(x) if not is_exists(x["id"]) else None, tweets)
         return 0
